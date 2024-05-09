d3f42633ed606e958eb6d3e8941ddc19c378ec8d
Alexander Desyatov <desyatovav@yandex.ru>
2024-05-09 13:20:35 +0300

    initial commit

---
+++ src/main/java/ru/yandex/practicum/catsgram/model/Cat.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.catsgram.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cat {
+    private String color;
+    private int age;
+}
